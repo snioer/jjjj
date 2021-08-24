d7625f18ad1bf1e0e3c6e3b45444c497369f1fee
snioer <45099729+snioer@users.noreply.github.com>
2021-08-24 21:02:29 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -23,15 +23,20 @@ jsproxy_config({
       label: 'cf',
       lines: {
         // 收费版（高权重）
-        'jsp.snioer.workers.dev/': 4,
-        //'node-cfworker-2.etherdream.com': 4,
+        'j.snioer.ml/': 4,
+        'js.snioer.ml/': 3,
+        'j.snioer.workers.dev/': 4,
+        'jsp.snioer.workers.dev/': 3,
+        'oracleusa.ml/': ,2
+        'node-cfworker-2.etherdream.com': ,1,
+
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        //'b.007.workers.dev': 1,
-        //'b.hehe.workers.dev': 1,
-        //'b.lulu.workers.dev': 1,
-        //'b.jsproxy.workers.dev': 1,
+        'b.007.workers.dev': 1,
+        'b.hehe.workers.dev': 1,
+        'b.lulu.workers.dev': 1,
+        'b.jsproxy.workers.dev': 1,
       }
     }
   },
